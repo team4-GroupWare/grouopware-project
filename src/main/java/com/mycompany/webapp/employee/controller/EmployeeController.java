a17b1427132dbf454b9d30925ee68b17820fcb06
LeeJiho0304 <leejiho0304@gmail.com>
2023-02-20 14:48:01 +0900

    head, header, footer 분리

--- src/main/java/com/mycompany/webapp/employee/controller/EmployeeController.java
+++ src/main/java/com/mycompany/webapp/employee/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package com.mycompany.webapp.employee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mycompany.webapp.employee.service.EmployeeService;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
 public class EmployeeController {
 
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@GetMapping("/department/list")
+	public String getDeptList() {
+		log.info("실행");
+		
+		return "hr/hr.jsp";
+	}
 }
