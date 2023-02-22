cb69807db7418070c9a516507d95ca198b598a23
ys189389 <ys189389@gmail.com>
2023-02-22 20:19:11 +0900

    merge

--- src/main/java/com/mycompany/webapp/employee/repository/EmployeeRepository.java
+++ src/main/java/com/mycompany/webapp/employee/repository/EmployeeRepository.java
@@ -9,11 +9,11 @@ import com.mycompany.webapp.employee.model.Employee;
 
 @Repository
 public interface EmployeeRepository {
-	public Employee selectByEmpId(String empId);
-	public List<Employee> selectEmpList(Pager pager);
-	public int selectEmpCount();
-	public int selectSearchEmpCount(Employee employee);
-	public int selectEmpId(String empId);
-	public int insertEmployee(Employee employee);
-	public int selectManagerNo(int empNo);
+	   public Employee selectByEmpId(String empId);
+	   public List<Employee> selectEmpList(Pager pager);
+	   public int selectEmpCount();
+	   public int selectSearchEmpCount(Employee employee);
+	   public int selectEmpId(String empId);
+	   public int insertEmployee(Employee employee);
+	   public int selectManagerNo(int empNo);
 }
