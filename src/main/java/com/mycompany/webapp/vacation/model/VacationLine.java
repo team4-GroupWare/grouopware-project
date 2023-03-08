146beb40a672be4ffb9fee97bc4744ea8e5e4b88
dlgld3 <dlgld3@naver.com>
2023-03-08 15:14:29 +0900

    휴가 작성 신청중

--- src/main/java/com/mycompany/webapp/vacation/model/VacationLine.java
+++ src/main/java/com/mycompany/webapp/vacation/model/VacationLine.java
@@ -7,4 +7,11 @@ public class VacationLine {
 	private int vacationLineId;
 	private int vacationId;
 	private String empId;
+	private int seq;
+	private boolean isApproved;
+	
+	private String empName;
+	private String teamName;
+	private String deptName;
+	private String gradeName;
 }
